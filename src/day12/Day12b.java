c4a03afa626cec95b30297ed411a226bf5ad4784
Mike Pereira <mike.pereiragoncalves@education.lu>
2026-06-10 00:56:39 +0200

    Solve day 12b

--- src/day12/Day12b.java
+++ src/day12/Day12b.java
@@ -1,6 +1,12 @@
 package day12;
 
 import day12.json.JSON;
+import day12.json.JsonAny;
+import day12.json.JsonArray;
+import day12.json.JsonNumber;
+import day12.json.JsonObject;
+import day12.json.JsonString;
+import day12.json.JsonAny.Type;
 
 /**
  *
@@ -8,17 +14,69 @@ import day12.json.JSON;
  */
 public class Day12b {
 
+    private static final String BLACKLISTED_WORD = "red";
+    
     public Day12b(String input) {
         JSON json = new JSON(input);
-        IO.println("Sum of all numbers outside of blacklisted JSON objects: " + crawler(json));
+        double sum = crawler(json.value);
+
+        IO.print("Sum of all numbers outside of blacklisted JSON objects: ");
+        if ((int) sum == sum) {
+            IO.println((int) sum);
+        } else {
+            IO.println((int) sum);
+        }
     }
-    
+
     /**
      * Traverses through all the JSON and adds up all numeric values, but skips
      * JSON objects that contain the value "red"
      */
-    private int crawler(JSON json) {
+    private double crawler(JsonAny any) {
+        if (any == null) {
+            IO.println("Invalid JSON");
+            return -1;
+        }
+
+        switch (any.type) {
+            case String, Boolean, Null -> {
+                return 0;
+            }
+            case Number -> {
+                return ((JsonNumber) any).value;
+            }
+            case Array -> {
+                int sum = 0;
+                JsonArray array = (JsonArray) any;
+                for (int i = 0; i < array.value.size(); i++) {
+                    sum += crawler(array.value.get(i));
+                }
+                return sum;
+            }
+            case Object -> {
+                JsonObject object = (JsonObject) any;
+                if (containsBlacklistedWord(object)) {
+                    return 0;
+                } else {
+                    int sum = 0;
+                    for (String key : object.value.keySet()) {
+                        sum += crawler(object.value.get(key));
+                    }
+                    return sum;
+                }
+            }
+        }
         return 0;
+    }
+
+    private boolean containsBlacklistedWord(JsonObject object) {
+        for (String key : object.value.keySet()) {
+            JsonAny any = object.value.get(key);
+            if(any.type == Type.String && ((JsonString)any).value.equals(BLACKLISTED_WORD)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
